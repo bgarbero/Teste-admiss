cc1c1832b976e80dc18655d9c18edaf92c42ec6b
bgarbero <bruno_garbero@hotmail.com>
2026-05-01 16:37:24 -0300

    feat: adiciona entidade Aeronave com mapeamento JPA

---
+++ backend/src/main/java/com/teste_admiss/infraestruture/domain/Aeronave.java
@@ -0,0 +1,44 @@
+package com.teste_admiss.infraestruture.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_aeronaves")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aeronave {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String marca;
+
+    @Column
+    private Integer ano;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private boolean vendido;
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    @Column
+    @UpdateTimestamp
+    private LocalDateTime updated;
+
+}
