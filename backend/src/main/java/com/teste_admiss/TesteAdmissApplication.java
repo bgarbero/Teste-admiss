576056d7e754e7113cd9573a9c8daa0d4995ea52
bgarbero <bruno_garbero@hotmail.com>
2026-05-01 16:13:42 -0300

    Initial commit

---
+++ backend/src/main/java/com/teste_admiss/TesteAdmissApplication.java
@@ -0,0 +1,13 @@
+package com.teste_admiss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAdmissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAdmissApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/teste_admiss/TesteAdmissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste_admiss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAdmissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
